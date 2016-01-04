67e9222bfd0b7c1c40ea07710b956ba90f332c6a
JasonMcTigue <jasonmctigue@live.ie>
2016-01-04 14:57:30 +0000

    Initial commit.

---
+++ Multi-threaded Cypher-Breaker/src/ie/gmit/sw/FileParser.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw;
+/*package gmit;
+
+import java.io.*; 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FileParser {
+
+	public Map<String, Double> parse(String)
+
+}
+*/
---
+++ Multi-threaded Cypher-Breaker/src/ie/gmit/sw/TextScorer.java
@@ -0,0 +1,54 @@
+package ie.gmit.sw;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class TextScorer {
+	public TextScorer(Map<String, Double> map) {
+		super();
+		this.map = map;
+	}
+
+	private Map<String, Double> map = new ConcurrentHashMap<String, Double>();
+	
+	//put into its own class
+	/*public  Map<String, Double> parse(String dile){
+		BufferedReader br= new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		String next= null;
+		
+		while((next=br.readLine())!=null){
+				// ADD EACH LINE TO THE PARSE
+				String [] stuff = next.split(" ");
+				map.put(Stuff[0]),stuff[1]);
+		}
+		
+	}*/
+	
+	public void TextScorer1(Map<String, Double> m){
+		this.map = m;
+	}
+	
+	public double getScore(String text){
+		double score = 0f;
+
+		for (int i = 0; i < text.length(); i++){
+			if (i + QuadGramMap.GRAM_SIZE <= text.length() -1){
+				score += computeLogScore(text.substring(i, i + 4));
+			}
+		}
+		return score;
+	}
+	
+	public double computeLogScore(String quadgram){
+		if (map.containsKey(quadgram)){
+			double frequency = map.get(quadgram);
+			double total = (double) map.size();
+			double probability = (double) (frequency/total);
+			
+			return Math.log10(probability);
+		}else{
+			return 0f;
+		}
+	}
+}
